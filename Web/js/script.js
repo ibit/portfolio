165c8fe4201f323bcc9eeaeab1f0c4c50b65c1e7
ibit <60405790+ibit@users.noreply.github.com>
2025-07-24 10:48:46 +0900

    Add files via upload

---
+++ Web/js/script.js
@@ -0,0 +1,8 @@
+// console message
+console.log('%c🚀 Welcome to the console of ibit.dev!', 'color: #ff8c00ff; background: #000000ff; font-size: 20px; padding: 6px; border-radius: 4px');
+
+//clear console
+window.clear = () => {
+    console.clear();
+    console.log('%c🧹 Console cleared!', 'color: #0be804ff; font-weight: bold; font-size: 18px; padding: 4px 0;');
+};
